d6db69b11e7580b5db183424bea8d9d3229835ab
Mark Mooney <mjmoon15@gmail.com>
2020-07-07 11:13:22 -0400

    built read/write new notes routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const { json } = require("body-parser");
 
 //express and ports
 const app = express();
@@ -22,6 +23,28 @@ app.get("/api/notes", (req, res) => {
     fs.readFile(__dirname, "public/db.json", "utf8", (err, data) => {
         if (err) throw err;
         return res.json(JSON.parse(data))
+    })
+})
+//add a new note
+    //read notes
+app.post("/api/notes", (req, res) => {
+    fs.readFile(__dirname, "/public/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+
+        //parse new data from JSON and set ids for new notes
+        const jsonData = JSON.parse(data);
+        const idArray = jsonData.map(note => note.id);
+        const biggestId = Math.max(...idArray);
+        const newNote = req.body;
+        newNote.id = biggestId + 1;
+        jsonData.push(newNote)
+
+        //write new
+        fs.writeFile(__dirname, "/public/db.json", JSON.stringify(jsonData), (err, data) => {
+            if (err) throw err;
+            res.json(jsonData);
+
+        })
     })
 })
 
